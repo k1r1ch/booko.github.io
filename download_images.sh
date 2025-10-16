513582f730e5a1c83c84979419e0782995ba1185
k1r1ch <76542381+k1r1ch@users.noreply.github.com>
2025-10-16 08:33:30 +0300

    Уе
    Р

---
+++ download_images.sh
@@ -0,0 +1,76 @@
+#!/bin/bash
+# Скрипт скачивает изображения товаров (запустите локально в папке проекта).
+# Требуется wget
+
+# Книга "Трубки" (581(з))
+# Откройте страницу: https://www.elite-book.ru/catalog/elitnyie-knigi/napitki-i-sigaryi/kniga-trubki
+# Сохраните основное изображение под: assets/img/581.jpg
+# Например (замените IMAGE_URL на реальную ссылку):
+# wget -O assets/img/581.jpg 'IMAGE_URL'
+
+# ВИСКИ. Атлас мира. Дейв Брум (606(зн))
+# Откройте страницу: https://www.elite-book.ru/catalog/elitnyie-knigi/napitki-i-sigaryi/viski-atlas-mira-dejv-brum
+# Сохраните основное изображение под: assets/img/606.jpg
+# Например (замените IMAGE_URL на реальную ссылку):
+# wget -O assets/img/606.jpg 'IMAGE_URL'
+
+# Книга "Вина мира" Путеводитель (584(з))
+# Откройте страницу: https://www.elite-book.ru/catalog/elitnyie-knigi/napitki-i-sigaryi/kniga-vina-mira
+# Сохраните основное изображение под: assets/img/584.jpg
+# Например (замените IMAGE_URL на реальную ссылку):
+# wget -O assets/img/584.jpg 'IMAGE_URL'
+
+# Книга "ПИВО" иллюстрированная энциклопедия (582(з))
+# Откройте страницу: https://www.elite-book.ru/catalog/elitnyie-knigi/napitki-i-sigaryi/kniga-pivo
+# Сохраните основное изображение под: assets/img/582.jpg
+# Например (замените IMAGE_URL на реальную ссылку):
+# wget -O assets/img/582.jpg 'IMAGE_URL'
+
+# Книга "Библия Бармена" (496(з))
+# Откройте страницу: https://www.elite-book.ru/catalog/elitnyie-knigi/napitki-i-sigaryi/kniga-bibliya-barmena
+# Сохраните основное изображение под: assets/img/496.jpg
+# Например (замените IMAGE_URL на реальную ссылку):
+# wget -O assets/img/496.jpg 'IMAGE_URL'
+
+# Книга "Крепкие спиртные напитки" (412(з))
+# Откройте страницу: https://www.elite-book.ru/catalog/elitnyie-knigi/napitki-i-sigaryi/kniga-krepkie-spirtnye-napitki
+# Сохраните основное изображение под: assets/img/412.jpg
+# Например (замените IMAGE_URL на реальную ссылку):
+# wget -O assets/img/412.jpg 'IMAGE_URL'
+
+# Книга "Кофе" торжество многообразия (441(з))
+# Откройте страницу: https://www.elite-book.ru/catalog/elitnyie-knigi/napitki-i-sigaryi/kniga-kofe
+# Сохраните основное изображение под: assets/img/441.jpg
+# Например (замените IMAGE_URL на реальную ссылку):
+# wget -O assets/img/441.jpg 'IMAGE_URL'
+
+# Книга "Водка путеводитель" (595(з))
+# Откройте страницу: https://www.elite-book.ru/catalog/elitnyie-knigi/napitki-i-sigaryi/kniga-vodka-putevoditel
+# Сохраните основное изображение под: assets/img/595.jpg
+# Например (замените IMAGE_URL на реальную ссылку):
+# wget -O assets/img/595.jpg 'IMAGE_URL'
+
+# Книга "Мир Вина. Вина, сорта, виноградники" (604(з))
+# Откройте страницу: https://www.elite-book.ru/catalog/elitnyie-knigi/napitki-i-sigaryi/kniga-mir-vina
+# Сохраните основное изображение под: assets/img/604.jpg
+# Например (замените IMAGE_URL на реальную ссылку):
+# wget -O assets/img/604.jpg 'IMAGE_URL'
+
+# Как пить и не выглядеть идиотом (659(з))
+# Откройте страницу: https://www.elite-book.ru/catalog/elitnyie-knigi/napitki-i-sigaryi/kak-pit-i-ne-vyglyadet-idiotom
+# Сохраните основное изображение под: assets/img/659.jpg
+# Например (замените IMAGE_URL на реальную ссылку):
+# wget -O assets/img/659.jpg 'IMAGE_URL'
+
+# Русская охота — Сабанеев Л.П. (656(з))
+# Откройте страницу: https://www.elite-book.ru/catalog/elitnyie-knigi/oruzhie-i-oxota/russkaya-ohota-sabaneev
+# Сохраните основное изображение под: assets/img/656.jpg
+# Например (замените IMAGE_URL на реальную ссылку):
+# wget -O assets/img/656.jpg 'IMAGE_URL'
+
+# Великие военачальники России (655(з))
+# Откройте страницу: https://www.elite-book.ru/catalog/elitnyie-knigi/istoriya/velikie-voenachalniki-rossii
+# Сохраните основное изображение под: assets/img/655.jpg
+# Например (замените IMAGE_URL на реальную ссылку):
+# wget -O assets/img/655.jpg 'IMAGE_URL'
+
